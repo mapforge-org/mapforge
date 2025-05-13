4c416429463b2b652a840ae3caf3093dde08f30d
Thomas Schmidt <tschmidt@suse.de>
2025-05-13 23:26:13 +0200

    check if track exists

--- engines/ulogger/app/controllers/tracks_controller.rb
+++ engines/ulogger/app/controllers/tracks_controller.rb
@@ -3,10 +3,14 @@ class TracksController < ApplicationController
     if params[:id]
       padded = "%024d" % [ params[:id] ]
       map = Map.find(padded)
-      track = map.features.line_string.first
       if map
         # link to highlighted track
-        redirect_to map_path(id: map.public_id, f: track.id)
+        track = map.features.line_string.first
+        if track
+          redirect_to map_path(id: map.public_id, f: track.id)
+        else
+          redirect_to map_path(id: map.public_id)
+        end
       else
         Rails.logger.warn("Ulogger map '#{params[:id]}' not found")
         render status: :not_found
