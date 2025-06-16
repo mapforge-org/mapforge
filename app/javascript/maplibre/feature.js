dbe18139308115db46c7cfb5abf20fdf3317d8cc
Thomas Schmidt <tschmidt@suse.de>
2025-06-16 23:35:53 +0200

    Update app/javascript/maplibre/feature.js
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- app/javascript/maplibre/feature.js
+++ app/javascript/maplibre/feature.js
@@ -187,8 +187,10 @@ export function highlightFeature (feature, sticky = false, source = 'geojson-sou
     stickyFeatureHighlight = sticky
     highlightedFeatureId = feature.id
     // load feature from source, the style only returns the dimensions on screen
-    const sourceFeature = layers.filter(l => l.geojson !== undefined)
-      .flatMap(layer => layer.geojson.features).find(f => f.id === feature.id)
+    const sourceFeature = layers
+      .filter(l => Array.isArray(l.geojson?.features))
+      .flatMap(layer => layer.geojson.features)
+      .find(f => f.id === feature.id)
 
     if (sourceFeature) {
       showFeatureDetails(sourceFeature)
