e164573d7ca57870a4d23678b1111eb6e31d1a23
Thomas Schmidt <tschmidt@suse.de>
2025-06-18 18:10:42 +0200

    improve layers dropdown

--- app/javascript/maplibre/controls/shared.js
+++ app/javascript/maplibre/controls/shared.js
@@ -105,8 +105,7 @@ export function initLayersModal () {
     e.innerHTML = ''
     const template = document.querySelector('#layer-item-template')
     let ul = document.querySelector('#layers-modal #query-dropdown')
-    ul.innerHTML = '<li data-query-name="Custom query">Custom query</li>'
-
+    ul.innerHTML = '<li data-query-name="Custom query">Custom query</li><li>-----</li>'
     queries.sort((a, b) => a.name.localeCompare(b.name)).forEach(q => {
       let li = document.createElement('li')
       li.dataset.queryName = q['name']
