b5bf5a40071a36caa1c22b55967c36e4754afc7d
Thomas Schmidt <tschmidt@suse.de>
2025-07-25 02:57:49 +0200

    use chart.js bundle from esm.sh

--- config/importmap.rb
+++ config/importmap.rb
@@ -40,6 +40,7 @@ pin "@maplibre/maplibre-gl-directions", preload: false, to: "@maplibre--maplibre
 pin "maplibre-contour", preload: false # @0.1.0
 
 # Elevation chart
+# from https://esm.sh/chart.js@4.5.0/es2022/chart.bundle.mjs
 pin "chart.js", to: "chart-js.js", preload: false # @4.5.0
 pin "@kurkle/color", to: "@kurkle--color", preload: false # @0.3.4
 
