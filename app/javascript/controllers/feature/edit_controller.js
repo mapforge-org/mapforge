a8db7797953d98a94847e5a5dd9376ee92d69daf
Thomas Schmidt <tschmidt@suse.de>
2025-07-28 17:35:51 +0200

    remove arrow icon on tracks, because its not rotated anymore

--- app/javascript/controllers/feature/edit_controller.js
+++ app/javascript/controllers/feature/edit_controller.js
@@ -158,7 +158,7 @@ export default class extends Controller {
     const feature = this.getFeature()
     if (document.querySelector('#show-km-markers').checked) {
       feature.properties['show-km-markers'] = true
-      feature.properties['stroke-image-url'] = "/icons/direction-arrow.png"
+      // feature.properties['stroke-image-url'] = "/icons/direction-arrow.png"
     } else {
       delete feature.properties['show-km-markers']
       delete feature.properties['stroke-image-url']
