573a3a363c74c53293b2d7ef95e04216458cdec7
Thomas Schmidt <tschmidt@suse.de>
2025-08-12 23:12:55 +0200

    register service worker

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,3 +17,14 @@ AOS.init({
   easing: 'ease-in-out',
   once: true
 })
+
+if ('serviceWorker' in navigator) {
+  // Register the service worker
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function (registration) {
+      console.log('Service Worker registered with scope:', registration.scope)
+    })
+    .catch(function (error) {
+      console.log('Service Worker registration failed:', error)
+    })
+}
