dfd7131087b801c2dcfca29d36cf41cdab1d3bc3
Thomas Schmidt <tschmidt@suse.de>
2025-08-16 23:48:02 +0200

    don't intercept events in service worker

--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -3,27 +3,10 @@
 
 // Add a service worker for processing Web Push notifications:
 
-self.addEventListener("push", async (event) => {
-  const { title, options } = await event.data.json()
-  event.waitUntil(self.registration.showNotification(title, options))
-})
+// Hook into web requests
+// self.addEventListener('fetch', function (event) {
+//   console.log('Fetch event for ', event.request.url)
 
-self.addEventListener("notificationclick", function(event) {
-  event.notification.close()
-  event.waitUntil(
-    clients.matchAll({ type: "window" }).then((clientList) => {
-      for (let i = 0; i < clientList.length; i++) {
-        let client = clientList[i]
-        let clientPath = (new URL(client.url)).pathname
+//   return fetch(event.request)
+// })
 
-        if (clientPath == event.notification.data.path && "focus" in client) {
-          return client.focus()
-        }
-      }
-
-      if (clients.openWindow) {
-        return clients.openWindow(event.notification.data.path)
-      }
-    })
-  )
-})
