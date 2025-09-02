59b5102a2644109e2f4cda52db9e84245d19992e
Thomas Schmidt <tschmidt@suse.de>
2025-09-02 11:41:14 +0200

    debug beta value

--- app/javascript/maplibre/controls/shared.js
+++ app/javascript/maplibre/controls/shared.js
@@ -273,8 +273,9 @@ export function initializeDefaultControls () {
           if (dot) {
             // Get latest screen-adjusted deviceorientation data
             let screenAdjustedEvent = orientationControl.getScreenAdjustedEuler()
+            console.log(screenAdjustedEvent)
             let heading
-            if (87 < Math.abs(screenAdjustedEvent.beta) < 93) {
+            if (screenAdjustedEvent.isAvailable(screenAdjustedEvent.BETA) && (87 < Math.abs(screenAdjustedEvent.beta) < 93)) {
               // when the phone is vertical, alpha is unreliable
             } else {
               heading = screenAdjustedEvent.alpha
