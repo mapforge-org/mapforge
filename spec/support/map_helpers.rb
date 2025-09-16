acd51e1a6014f32cc11911677f717ced8a097911
Thomas Schmidt <tschmidt@suse.de>
2025-09-16 02:20:09 +0200

    extend wait time for map data load

--- spec/support/map_helpers.rb
+++ spec/support/map_helpers.rb
@@ -3,11 +3,11 @@
 # * GeoJSON features loaded + drawn
 # * Websocket is established
 def expect_map_loaded
-  expect(page).to have_css("#maplibre-map[data-map-loaded='true']")
-  expect(page).to have_css("#maplibre-map[data-geojson-loaded='true']")
+  expect(page).to have_css("#maplibre-map[data-map-loaded='true']", wait: 20)
+  expect(page).to have_css("#maplibre-map[data-geojson-loaded='true']", wait: 20)
   expect(page).to have_css("#maplibre-map[data-online='true']")
 end
 
 def expect_overpass_loaded
-  expect(page).to have_css("#maplibre-map[data-overpass-loaded='true']")
+  expect(page).to have_css("#maplibre-map[data-overpass-loaded='true']", wait: 30)
 end
