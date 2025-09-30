2d49f8fd2349f104ddfac16fd4738d9588de5928
Thomas Schmidt <tschmidt@suse.de>
2025-09-30 22:46:08 +0200

    implement websocket monitoring

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,21 +1,17 @@
-Yabeda.counter(:websocket_connections_total, comment: "Total websocket connections")
-Yabeda.gauge(:websocket_active_connections, comment: "Currently active websocket connections")
-
 class ApplicationCable::Connection < ActionCable::Connection::Base
-  identified_by :current_user
-
-  def connect2
-    Yabeda.websocket_connections_total.increment
-    Yabeda.websocket_active_connections.set(
-      nil,
-      Yabeda.websocket_active_connections.get(nil).to_i + 1
-    )
+  def connect
+    Yabeda.websocket_connections_opened.increment({})
+    Yabeda.websocket_active_connections.set({}, current_active_connections + 1)
   end
 
-  def disconnect2
-    Yabeda.websocket_active_connections.set(
-      nil,
-      [ Yabeda.websocket_active_connections.get(nil).to_i - 1, 0 ].max
-    )
+  def disconnect
+    Yabeda.websocket_connections_closed.increment({})
+    Yabeda.websocket_active_connections.set({}, current_active_connections - 1)
+  end
+
+  private
+
+  def current_active_connections
+    ActionCable.server.connections.size
   end
 end
