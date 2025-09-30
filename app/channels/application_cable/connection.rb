ea8f22f889cf31d245cd7b6956a2bda015441c00
Thomas Schmidt <tschmidt@suse.de>
2025-09-30 23:39:03 +0200

    add yabeda/prometheus exporters (#445)
    * add yabeda/prometheus exporters
    
    * upadate package lists
    
    * implement websocket monitoring
    
    * implement websocket monitoring

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
-module ApplicationCable
-  class Connection < ActionCable::Connection::Base
+class ApplicationCable::Connection < ActionCable::Connection::Base
+  def connect
+    Yabeda.websocket_connections_opened.increment({})
+    Yabeda.websocket_active_connections.set({}, current_active_connections + 1)
+  end
+
+  def disconnect
+    Yabeda.websocket_connections_closed.increment({})
+    Yabeda.websocket_active_connections.set({}, current_active_connections - 1)
+  end
+
+  private
+
+  def current_active_connections
+    ActionCable.server.connections.size
   end
 end
