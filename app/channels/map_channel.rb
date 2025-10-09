1b5387474563de4bc8390919e218a5f1f8190d40
Thomas Schmidt <tschmidt@suse.de>
2025-10-09 10:26:45 +0200

    Fix nocov

--- app/channels/map_channel.rb
+++ app/channels/map_channel.rb
@@ -96,7 +96,9 @@ class MapChannel < ApplicationCable::Channel
     if img = Image.find_by(public_id:)
       @feature.update!(image: img)
     else
-      Rails.logger.info "Cannot associate image object '#{public_id}' to feature, not hosted on mapforge" # :nocov:
+      # :nocov:
+      Rails.logger.info "Cannot associate image object '#{public_id}' to feature, not hosted on mapforge"
+      # :nocov:
     end
   end
 end
