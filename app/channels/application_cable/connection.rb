9e750f3f547d0b069ac3c656aa7213a409495812
Thomas Schmidt <tschmidt@suse.de>
2025-10-27 00:12:21 +0100

    asign a connection id

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,10 +1,14 @@
 class ApplicationCable::Connection < ActionCable::Connection::Base
+  identified_by :uuid
+
   def connect
+    self.uuid = SecureRandom.uuid
     Yabeda.websocket.connections_opened.increment({})
     Yabeda.websocket.active_connections.set({}, current_active_connections + 1)
   end
 
   def disconnect
+    self.uuid = nil
     Yabeda.websocket.connections_closed.increment({})
     Yabeda.websocket.active_connections.set({}, [ current_active_connections - 1, 0 ].max)
   end
