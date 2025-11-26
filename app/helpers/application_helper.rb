7ef3a2d075d3a2fc2819ac5100bc18ce7e414667
Thomas Schmidt <tschmidt@suse.de>
2025-11-26 22:46:01 +0100

    use small avatar previews

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def avatar_url(base_url, size)
+    uri = URI(base_url)
+    params = URI.decode_www_form(uri.query.to_s)
+    params.reject! { |k, _| k == "s" }
+    params << [ "s", size.to_s ]
+    uri.query = URI.encode_www_form(params)
+    uri.to_s
+  end
 end
