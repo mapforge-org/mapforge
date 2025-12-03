9c8eab570206a7b04a86561b5e7acca18eebba19
Thomas Schmidt <tschmidt@suse.de>
2025-12-03 14:53:30 +0100

    improve stimulus sort dropdown

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -1,19 +1,40 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+
+  static values = {
+    sort: String,
+    sortLabel: String,
+    direction: String
+  }
+
   async connect () {
   }
 
-  async loadMaps(event) {
-    const url = `/admin?sort=${event.params.sort}&direction=${event.params.direction}`
+  changeSort(event) {
+    this.sortValue = event.params.sort || this.sortValue
+    this.sortLabelValue = event.params.sortLabel || this.sortLabelValue
 
+    this.loadMaps(event)
+    document.getElementById('sortTitle').innerText = this.sortLabelValue
+  }
+
+  changeDirection(event) {
+    this.directionValue = event.params.direction || this.directionValue
+    
+    this.loadMaps(event)
+    let sortIcon = document.getElementById('sortIcon')
+    sortIcon.classList.remove('bi-sort-down', 'bi-sort-up')
+    this.directionValue === 'desc' ? sortIcon.classList.add('bi-sort-down') : sortIcon.classList.add('bi-sort-up')
+  }
+
+  async loadMaps(_event) {
+    const url = `/admin?sort=${this.sortValue}&direction=${this.directionValue}`
     const response = await fetch(url, {
       headers: { "Accept": "text/vnd.turbo-stream.html" }
     })
-
     const html = await response.text()
     Turbo.renderStreamMessage(html)
-
     // Optionally update URL in address bar without full reload
     window.history.replaceState({}, "", url)
   }
