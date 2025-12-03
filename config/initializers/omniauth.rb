d35e266580d3dccb11f98825fc72d29caf2f142b
Thomas Schmidt <tschmidt@suse.de>
2025-12-03 11:45:21 +0100

    use correct csrf key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -12,5 +12,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
             prompt: "select_account"
 end
 
-OmniAuth.config.request_validation_phase = OmniAuth::AuthenticityTokenProtection.new
+OmniAuth.config.request_validation_phase = OmniAuth::AuthenticityTokenProtection.new(key: :_csrf_token)
 OmniAuth.config.logger = Rails.logger
