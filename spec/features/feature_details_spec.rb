2250dbb1f08d45698bfca641a37e072d0500b060
Thomas Schmidt <tschmidt@suse.de>
2025-12-29 21:18:52 +0100

    separate mobile tests

--- spec/features/feature_details_spec.rb
+++ spec/features/feature_details_spec.rb
@@ -8,7 +8,7 @@ describe 'Feature details' do
     expect_map_loaded
   end
 
-  context 'with polygon on map' do
+  context 'mobile', :mobile do
     before { create(:feature, :polygon_middle, layer: map.layers.first, title: 'Poly Title') }
 
     context 'with selected feature' do
@@ -17,7 +17,7 @@ describe 'Feature details' do
         expect(page).to have_css('#feature-details-modal')
       end
 
-      it 'can enlarge modal with pull-up button', :mobile do
+      it 'can enlarge modal with pull-up button' do
         height = element_offset_height('#feature-details-modal')
         # initial height is half the screen height
         expect(height).to be < 300
@@ -25,6 +25,17 @@ describe 'Feature details' do
         sleep(0.3)
         height = element_offset_height('#feature-details-modal')
         expect(height).to be > 300
+      end
+    end
+  end
+
+  context 'export' do
+    before { create(:feature, :polygon_middle, layer: map.layers.first, title: 'Poly Title') }
+
+    context 'with selected feature' do
+      before do
+        click_center_of_screen
+        expect(page).to have_css('#feature-details-modal')
       end
 
       it 'can download feature export' do
