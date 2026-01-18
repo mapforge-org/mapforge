1923bb1c0f75a36d104de406a098afe4dfb46b2d
Thomas Schmidt <tschmidt@suse.de>
2026-01-18 02:05:26 +0100

    offer doc pages as markdown

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -4,7 +4,12 @@ class DocsController < ApplicationController
 
   def tutorial
     tutorial = Tutorial.find(params[:id])
-    @title = tutorial.title
-    @html_content = tutorial.to_html
+    respond_to do |format|
+      format.html do
+        @title = tutorial.title
+        @html_content = tutorial.to_html
+      end
+      format.md { render markdown: tutorial.markdown }
+    end
   end
 end
