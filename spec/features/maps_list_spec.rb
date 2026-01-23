c9e12dc5cf3dfa2a6df93e7e7dc89fb1fe07043a
Thomas Schmidt <tschmidt@suse.de>
2026-01-23 23:45:24 +0100

    earch specs

--- spec/features/maps_list_spec.rb
+++ spec/features/maps_list_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe 'Map List' do
-  let!(:maps) { create_list(:map, 3, view_permission: 'listed') }
+  let(:maps) { create_list(:map, 3, view_permission: 'listed') }
   let(:user) { create(:user) }
 
   before do
+    maps
     visit maps_path
   end
 
@@ -21,15 +22,35 @@ describe 'Map List' do
     expect(page).to have_selector(:xpath, "//a[@href='/m/#{new_map.public_id}/broadcast']")
   end
 
-  context 'for user (/my)' do
-    before do
-      allow_any_instance_of(ApplicationController).to receive(:session).and_return({ user_id: user.id })
-      user.maps << maps.first
-      visit my_path
+  context 'filter list' do
+    let(:maps) { [ create(:map, name: 'Map1', view_permission: 'listed'),
+                   create(:map, name: 'Map2', user: user, view_permission: 'listed') ] }
+
+    it 'searches in map names' do
+      fill_in "search", with: "Map1"
+      find_field("search").send_keys(:enter)
+
+      expect(page).to have_selector('.map-preview', count: 1)
+      expect(page).to have_text('Map1')
+      expect(page).not_to have_text('Map2')
     end
 
-    it 'shows private links to maps' do
-      expect(page).to have_selector(:xpath, "//a[@href='/m/#{maps[0].private_id}']")
+    it 'can clear filter' do
+      fill_in "search", with: "Map1"
+      find_field("search").send_keys(:enter)
+
+      expect(page).to have_selector('.map-preview', count: 1)
+      find("button[aria-label='Clear filter']").click
+      expect(page).to have_selector('.map-preview', count: 2)
+    end
+
+    it 'searches for map owners' do
+      fill_in "search", with: "user:#{user.id}"
+      find_field("search").send_keys(:enter)
+
+      expect(page).to have_selector('.map-preview', count: 1)
+      expect(page).to have_text('Map2')
+      expect(page).not_to have_text('Map1')
     end
   end
 end
--- spec/features/maps_my_list_spec.rb
+++ spec/features/maps_my_list_spec.rb
@@ -9,6 +9,10 @@ describe 'Map List' do
     visit my_path
   end
 
+  it 'shows private links to maps' do
+      expect(page).to have_selector(:xpath, "//a[@href='/m/#{user.maps.first.private_id}']")
+  end
+
   it 'can delete own map' do
     expect(user.maps_count).to eq 1
     expect(page).to have_css(".map-preview")
@@ -17,5 +21,19 @@ describe 'Map List' do
     end
     expect(page).not_to have_css(".map-preview")
     expect(user.reload.maps_count).to eq 0
+  end
+
+  context 'filter list' do
+    before { [ create(:map, user: user, name: 'Map1'),
+                    create(:map, user: user, name: 'Map2') ] }
+
+    it 'searches in map names' do
+      fill_in "search", with: "Map1"
+      find_field("search").send_keys(:enter)
+
+      expect(page).to have_selector('.map-preview', count: 1)
+      expect(page).to have_text('Map1')
+      expect(page).not_to have_text('Map2')
+    end
   end
 end
