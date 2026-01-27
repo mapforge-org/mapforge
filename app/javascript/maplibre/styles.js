fff1ac3ee4ad6e77b3e1c9c7a4f509d720180702
Thomas Schmidt <tschmidt@suse.de>
2026-01-28 00:57:57 +0100

    Reapply "support minzoom property to hide features when zooming out"
    This reverts commit 0be53b57598e0ccd8ec1e95ed9f2331216544909.

--- app/javascript/maplibre/styles.js
+++ app/javascript/maplibre/styles.js
@@ -388,13 +388,19 @@ export function styles () {
     'points-layer-flat': {
       id: 'points-layer-flat',
       type: 'circle',
-      filter: ['all',
-        ['==', '$type', 'Point'],
-        ['!=', 'meta', 'midpoint'],
-        ['!=', 'meta', 'vertex'],
-        ['has', 'flat'],
-        ['!has', 'point_count'],
-        ["!has", "heatmap"]
+      filter: [
+        "all",
+        ["==", ["geometry-type"], "Point"],
+        ["!=", ["get", "meta"], "midpoint"],
+        ["!=", ["get", "meta"], "vertex"],
+        ["has", "flat"],
+        ["!", ["has", "point_count"]],
+        ["!", ["has", "heatmap"]],
+        [
+          ">=",
+          ["zoom"],
+          ["coalesce", ["to-number", ["get", "minzoom"]], 0]
+        ]
       ],
       paint: {
         "circle-pitch-alignment": "map",
@@ -439,13 +445,19 @@ export function styles () {
     'points-layer': {
       id: 'points-layer',
       type: 'circle',
-      filter: ['all',
-        ['==', '$type', 'Point'],
-        ['!=', 'meta', 'midpoint'],
-        ['!=', 'meta', 'vertex'],
-        ['!has', 'flat'],
-        ['!has', 'point_count'], 
-        ["!has", "heatmap"]
+      filter: [
+        "all",
+        ["==", ["geometry-type"], "Point"],
+        ["!=", ["get", "meta"], "midpoint"],
+        ["!=", ["get", "meta"], "vertex"],
+        ["!", ["has", "flat"]],
+        ["!", ["has", "point_count"]],
+        ["!", ["has", "heatmap"]],
+        [
+          ">=",
+          ["zoom"],
+          ["coalesce", ["to-number", ["get", "minzoom"]], 0]
+        ]
       ],
       paint: {
         'circle-pitch-scale': 'map', // points get bigger when camera is closer
@@ -490,12 +502,18 @@ export function styles () {
     'points-hit-layer': {
       id: 'points-hit-layer',
       type: 'circle',
-      filter: ['all',
-        ['==', '$type', 'Point'], 
-        ["!has", "ulogger-waypoint"]
+      filter: [
+        "all",
+        ["==", ["geometry-type"], "Point"],
+        [
+          ">=",
+          ["zoom"],
+          ["coalesce", ["to-number", ["get", "minzoom"]], 0]
+        ]
       ],
       paint: {
         'circle-radius': ['+', 5, pointSizeMax],
+        // 'circle-opacity': 0.3 // debug click area
         'circle-opacity': 0
       }
     },
