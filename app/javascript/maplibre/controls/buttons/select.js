4ed0161a4d9e8a6b601c9c7eb18486f9aebf750f
Thomas Schmidt <tschmidt@suse.de>
2026-05-11 12:44:39 +0200

    Reapply "debug map freeze - manual restore"
    This reverts commit 8b88aba4b919e21165530e6bf98c571a2320da93.

--- app/javascript/maplibre/controls/buttons/select.js
+++ app/javascript/maplibre/controls/buttons/select.js
@@ -1,11 +1,13 @@
 import { resetEditControls } from 'maplibre/controls/edit';
 import { resetControls } from 'maplibre/controls/shared';
+import { recoverHandlers } from 'maplibre/map';
 
 export class MapSelectControl {
   constructor (_options) {
     this._container = document.createElement('div')
     this._container.innerHTML = '<button class="maplibregl-ctrl-btn maplibregl-ctrl-select" type="button" title="Select mode" aria-label="Select mode" aria-pressed="false"><b><i class="bi bi-hand-index"></i></b></button>'
     this._container.onclick = function (e) {
+      recoverHandlers()
       resetControls()
       resetEditControls()
       e.target.closest('button').classList.add('active')
