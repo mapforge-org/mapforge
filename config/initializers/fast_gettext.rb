b2135e267cfe129692565035d6d545eeb7ade7d2
Thomas Schmidt <tschmidt@suse.de>
2026-07-19 11:14:47 +0200

    init gettext

---
+++ config/initializers/fast_gettext.rb
@@ -0,0 +1,9 @@
+FastGettext.add_text_domain "app", path: "locale", type: :po
+
+# "en" needs no locale/en/app.po file, so it is added explicitly here.
+FastGettext.default_available_locales =
+  ([ "en" ] + Dir.glob(Rails.root.join("locale", "*/")).map { |dir| File.basename(dir) }).uniq
+FastGettext.default_text_domain = "app"
+
+# Keep source locations (#: file:line) in the extracted .pot/.po files.
+Rails.application.config.gettext_i18n_rails.default_options = %w[--sort-by-msgid --no-wrap]
