a50899578faa9a2b405c0d68c134249182a174bb
Thomas Schmidt <tschmidt@suse.de>
2026-08-07 17:03:40 +0200

    reset change

--- vendor/javascript/@turf--center.js
+++ vendor/javascript/@turf--center.js
@@ -1,2 +1,4 @@
+// @turf/center@7.4.0 downloaded from https://ga.jspm.io/npm:@turf/center@7.4.0/dist/esm/index.js
+
 import{bbox as e}from"@turf/bbox";import{point as t}from"@turf/helpers";function n(n,r={}){let i=e(n);return t([(i[0]+i[2])/2,(i[1]+i[3])/2],r.properties,r)}var r=n;export{n as center,r as default};
 
